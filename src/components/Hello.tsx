423ac73df96f32f6a6804f213893854c37af0e2e
otofu-square <yukita@feedforce.jp>
2017-06-15 00:30:52 +0900

    :+1: Add MainPage component

--- src/components/Hello.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export interface HelloProps {
-  compiler: string;
-  framework: string;
-}
-
-export const Hello = ({ compiler, framework }: HelloProps) =>
-  <h1>Hello from {compiler} and {framework}!</h1>;
