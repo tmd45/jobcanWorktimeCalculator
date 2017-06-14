b2f90b9b94e20a4cb37b9f1eedcff783dab3f1df
otofu-square <yukita@feedforce.jp>
2017-06-15 01:58:35 +0900

    :shirt: Add Material-UI component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as injectTapEventPlugin from "react-tap-event-plugin";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("example"));
+injectTapEventPlugin();
+
+const Root = () =>
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>;
+
+ReactDOM.render(<Root />, document.getElementById("example"));
