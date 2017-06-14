232eabb00f99a87565e23dff5eced6de1aca5268
otofu-square <yukita@feedforce.jp>
2017-06-14 23:40:44 +0900

    :+1: Add build configurations

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist/js"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  }
+};
