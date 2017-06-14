723bdab679295d81858a31166b86e4480809c958
otofu-square <yukita@feedforce.jp>
2017-06-15 00:43:07 +0900

    :recycle: Refactor by split components

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 
+import Notification from "./Notification";
+import DefinitionList from "./DefinitionList";
+
 export interface HelloProps {
   compiler: string;
   framework: string;
@@ -8,42 +11,8 @@ export interface HelloProps {
 const MainPage = () =>
   <div>
     <h3>Calculate Working Time</h3>
-
-    <p className="notify">
-      値の取得・更新をするためには{" "}
-      <a href="https://ssl.jobcan.jp/employee/attendance" target="_blank">
-        JOBCAN の出勤簿ページ
-      </a>{" "}
-      へのアクセスが必要です。
-    </p>
-    <p className="notify">JOBCAN で「勤務中」にデータ取得を行った場合、貯金が大きくマイナスになる場合があります。</p>
-
-    <dl>
-      <hr />
-      <dt id="stdWorkDays">所定労働日数 <small>days</small></dt>
-      <dd />
-      <dt id="stdWorkHours">所定労働時間 <small>hours</small></dt>
-      <dd />
-      <hr />
-      <dt id="workedDays">実働日数 <small>days</small></dt>
-      <dd />
-      <dt id="workedHours">実労働時間 <small>hours</small></dt>
-      <dd />
-      <dt id="salariedDays">有給休暇消化日数 <small>days</small></dt>
-      <dd />
-      <hr />
-      <dt id="remainWorkDays">今月の残り出勤可能日数 <small>days</small></dt>
-      <dd />
-      <dt id="excessWorkTimes">給与労働時間 <small>hours</small></dt>
-      <dd />
-      <dt id="workTimeMargin">何時間余裕があるか（貯金） <small>hours</small></dt>
-      <dd />
-      <dt id="requiredWorkTimes">1日あたりの労働時間目安 <small>hours</small></dt>
-      <dd />
-      <hr />
-      <dt id="lastUpdatedAt">最終更新日時</dt>
-      <dd />
-    </dl>
+    <Notification />
+    <DefinitionList />
   </div>;
 
 export default MainPage;
---
+++ src/components/Notification.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+const Notification = () =>
+  <div>
+    <p className="notify">
+      値の取得・更新をするためには{" "}
+      <a href="https://ssl.jobcan.jp/employee/attendance" target="_blank">
+        JOBCAN の出勤簿ページ
+      </a>{" "}
+      へのアクセスが必要です。
+    </p>
+    <p className="notify">JOBCAN で「勤務中」にデータ取得を行った場合、貯金が大きくマイナスになる場合があります。</p>
+  </div>;
+
+export default Notification;
